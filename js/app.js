29aa36bd678886ea90c5e0f8da9d87de0e06a9bb
Száraz Ferenc <szarazferenc@SZFMacPro.local>
2020-10-05 18:10:30 +0200

    pwa app sources

--- js/app.js
+++ js/app.js
@@ -217,3 +217,11 @@ $(document).ready(function() {
     });
 
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('../sw.js').then(() => {
+            console.log('Service Worker Registered')
+        })
+    })
+}
